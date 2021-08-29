07a34121fa822e1174075d87b92b00efe92f1ecd
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2021-08-29 12:46:48 +1000

    Added the on_ready() event for testing purposes

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ from discord.ext import commands
 dotenv.load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-bot = commands.Bot(command_prefix="f!")
+bot = commands.Bot(command_prefix="f")
+
+@bot.event
+async def on_ready():
+  """
+  Indication of whether or not the bot has activated.
+  """
+  print(f"{bot.user} has connected to Discord!")
+  print("And connected to the servers:")
+  for guild in bot.guilds:
+    print(f"\t{guild.name}")
 
 @bot.command(name="echo")
 async def echo(context, *, arg):
